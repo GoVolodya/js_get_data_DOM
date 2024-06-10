87f0f98ea0304f55eb29dfc8520045933894cd17
Volodymyr Isaiev <isaiev.v.o@gmail.com>
2024-06-10 13:01:45 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const populationElements = document.querySelectorAll(".population");
+const populationData = [];
+populationElements.forEach((item)=>{
+    populationData.push(parseInt(item.textContent.replaceAll(",", "")));
+});
+const totalPopulation = populationData.reduce((accumulator, population)=>accumulator + population, 0);
+const averagePopulation = totalPopulation / populationData.length;
+document.querySelector(".average-population").textContent = averagePopulation.toLocaleString();
+document.querySelector(".total-population").textContent = totalPopulation.toLocaleString();
+
+//# sourceMappingURL=index.f75de5e1.js.map
